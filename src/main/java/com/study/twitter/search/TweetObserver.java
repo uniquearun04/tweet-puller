4dabfc9b0f1f723813563ec74bf6dfc19b729d9e
arun.chaudhary <arun.chaudhary@timesinternet.in>
2015-04-06 03:23:57 +0530

    changed to use blocking queue instead

--- src/main/java/com/study/twitter/search/TweetObserver.java
+++ src/main/java/com/study/twitter/search/TweetObserver.java
@@ -6,9 +6,10 @@ package com.study.twitter.search;
  * 
  * This class maintains the current tweets with geolocation property, which are used to draw on the map.
  */
-import java.util.Iterator;
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 import org.apache.log4j.Logger;
 
@@ -19,54 +20,75 @@ public class TweetObserver implements Observer {
 
 	private static final Logger logger = Logger.getLogger(TweetObserver.class);
 
-	public LinkedList<Status> getTweetList() {
+	public List<Status> getTweetList() {
+		List<Status> tweetList = new ArrayList<Status>();
+		for(int i = 0; i < tweetQueue.size(); i++){
+			try {
+				tweetList.add(tweetQueue.take());
+			} catch (InterruptedException e) {
+				logger.error(e,e);
+			}
+		}
 		return tweetList;
 	}
 
-	private LinkedList<Status> tweetList;
+//	private LinkedList<Status> tweetList;
+
+	private BlockingQueue<Status> tweetQueue;
 	private final int tweetLimit = 100;
 	private final int removeTweetItems = 10;
 	
 	private Subject tweetManager;
 	
 	public TweetObserver(Subject tweetManager) {
-		tweetList = new LinkedList<Status>();
+//		tweetList = new LinkedList<Status>();
+		tweetQueue = new LinkedBlockingQueue<Status>();
 		this.tweetManager = tweetManager;
 		tweetManager.register(this);
 	}
 	
 	@Override
 	public void update(List<Status> statusList) {
-		tweetList.addAll(statusList);
-		maintainList();
+		for(Status status : statusList){
+			if(status.getGeoLocation() != null){
+//				tweetList.addLast(status);
+				try {
+					tweetQueue.put(status);
+				} catch (InterruptedException e) {
+					logger.error(e, e);
+				}
+			}
+		}
+//		maintainList();
 //		printTweets();
 	}
 
 	@Override
 	public void update(Status status) {
-		tweetList.addLast(status);
-		maintainList();
+		if(status.getGeoLocation() != null){
+//			tweetList.addLast(status);
+			try {
+				tweetQueue.put(status);
+			} catch (InterruptedException e) {
+				logger.error(e, e);
+			}
+		}
+//		maintainList();
 //		printTweets();
 	}
 
 	
-	private void maintainList(){
-		for(Iterator<Status> it = tweetList.iterator(); it.hasNext();){
-			Status next = it.next();
-			if(next.getGeoLocation() == null){
-				it.remove();
-			}
-		}
-		if(tweetList.size() > tweetLimit){
-			for(int i = 0; i < removeTweetItems; i++){
-				tweetList.remove();
-			}
-		}
-	}
-	
-	private void printTweets(){
-		for(Status status : tweetList){
-			logger.info(status.getId()+", "+status.getText()+", "+status.getUser());
-		}
-	}
+//	private void maintainList(){
+//		if(tweetList.size() > tweetLimit){
+//			for(int i = 0; i < removeTweetItems; i++){
+//				tweetList.remove();
+//			}
+//		}
+//	}
+//	
+//	private void printTweets(){
+//		for(Status status : tweetList){
+//			logger.info(status.getId()+", "+status.getText()+", "+status.getUser());
+//		}
+//	}
 }
